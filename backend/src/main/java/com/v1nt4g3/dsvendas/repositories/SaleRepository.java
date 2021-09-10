ea9269221c42c5f0f8953b9c08254506b1c116b5
v1nt4g3 <marcos.vinicius.r.alencar@gmail.com>
2021-09-10 14:23:35 -0300

    Pagination

---
+++ backend/src/main/java/com/v1nt4g3/dsvendas/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.v1nt4g3.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.v1nt4g3.dsvendas.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+}
